967d057c72c24de92e86ce7f2148ef7efe0725b3
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-05-06 16:57:51 -0700

    component and template string

---
+++ src/components/video-detail.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const VideoDetail = ({video}) => {
+  const videoId = video.id.videoId;
+  const url = `http://www.youtube.com/embed/${videoId}`;
+
+  return(
+    <div className='video-detail col-md-8'>
+      <div className='embed-responive embed-responive-16by9'>
+        <iframe className='embed-responive-item' src={url}></iframe>
+      </div>
+      <div className='details'>
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.description}</div>
+      </div>
+    </div>
+  )
+};
+
+export default VideoDetail;
