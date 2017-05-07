75a50c8de598467429f1ee362ac2b5d9cd7157fc
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-05-06 18:02:13 -0700

    searching for videos

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -13,14 +13,18 @@ constructor(props){
   render() {
     return (
       <div className='search-bar'>
-        {this.state.term}
         <input
         value={this.state.term}
-        onChange={event => this.setState({term: event.target.value})}/>
+        onChange={event => this.onInputChange(event.target.value)}/>
       </div>
-  );
-}
-
+    );
   }
+
+  onInputChange(term){
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+
+}
 
 export default SearchBar;
